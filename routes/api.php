7b640fd9c70f73472b8f53b8349a1a431b6bf926
Cleber Boni <bonifaciocleber@gmail.com>
2021-04-30 23:26:24 -0300

    init controllers and models

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::apiResource('user', 'UserController');
+// Route::apiResource('user_category', 'UserCategoryController')->only(['index']);
+// Route::apiResource('wallet', 'WalletController')->only(['show', 'update']);
+// Route::apiResource('transaction', 'TransactionController')->only(['index', 'store', 'show']);
